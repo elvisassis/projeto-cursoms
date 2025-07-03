97d82d1bcc1a393fa9ff47bf9daab091037ee353
Elvis Assis <kesleiassis@gmail.com>
2025-07-03 11:27:25 -0300

    Criaçao MS de cartoes

---
+++ src/main/java/br/com/elvisassis/mscartoes/exception/ResourceNotFoundException.java
@@ -0,0 +1,1 @@
+package br.com.elvisassis.mscartoes.exception;public class ResourceNotFoundException extends RuntimeException {    public ResourceNotFoundException(String message) {        super(message);    }}
---
+++ src/main/java/br/com/elvisassis/mscartoes/exception/handler/ExceptionResponse.java
@@ -0,0 +1,1 @@
+package br.com.elvisassis.mscartoes.exception.handler;import java.util.Date;public record ExceptionResponse(Date timeStamp, String message, String details) {}
