1428c908712d6337675e4833efe9981b6f89bb22
Elvis Assis <kesleiassis@gmail.com>
2025-07-04 15:58:14 -0300

    Configuração Server Eureka

---
+++ src/test/java/br/com/elvisassis/eurikaserver/EurikaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elvisassis.eurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
