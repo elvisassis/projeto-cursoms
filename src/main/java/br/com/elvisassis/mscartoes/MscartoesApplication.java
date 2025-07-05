19766ce93d21dee1819beebc9a10c2983a0a7a26
Elvis Assis <kesleiassis@gmail.com>
2025-07-05 12:03:34 -0300

    Adicionando Log

--- src/main/java/br/com/elvisassis/mscartoes/MscartoesApplication.java
+++ src/main/java/br/com/elvisassis/mscartoes/MscartoesApplication.java
@@ -1,14 +1,18 @@
 package br.com.elvisassis.mscartoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @EnableRabbit
+@Slf4j
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
 
