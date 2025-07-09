ede12deacf7f05134948c9c7f984eef6134d4474
Elvis Assis <kesleiassis@gmail.com>
2025-07-09 10:27:09 -0300

    Configuração Dockerfile e variáveis de ambiente

--- src/main/java/br/com/elvisassis/msavaliadorcredito/CreditEvaluatorApplication.java
+++ src/main/java/br/com/elvisassis/msavaliadorcredito/CreditEvaluatorApplication.java
@@ -1,5 +1,6 @@
 package br.com.elvisassis.msavaliadorcredito;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CreditEvaluatorApplication {
 
 	public static void main(String[] args) {
+		// Carrega as variáveis do .env e as define como propriedades do sistema
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(CreditEvaluatorApplication.class, args);
 	}
 
