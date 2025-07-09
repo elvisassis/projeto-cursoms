c71302f0f80561da8170c44aee67b392e65c20a2
Elvis Assis <kesleiassis@gmail.com>
2025-07-09 10:23:27 -0300

    Configuração variavéis de ambiente

--- src/main/java/br/com/elvisassis/mscartoes/MscartoesApplication.java
+++ src/main/java/br/com/elvisassis/mscartoes/MscartoesApplication.java
@@ -1,5 +1,6 @@
 package br.com.elvisassis.mscartoes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
+
+		// Carrega as variáveis do .env e as define como propriedades do sistema
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
